ab856d804354d0e5c326aac2b54e78a1d577b1d3
Luigi Mateus <87377118+luigiikk@users.noreply.github.com>
2025-10-01 22:59:58 -0300

    first commit 🎉

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Button from "../../components/Button/button"
+
+function Home() {
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline"> Home</h1>
+        <Button/>
+    </>
+  )
+}
+
+export default Home
