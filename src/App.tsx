9c2d7712de58e05f2e648c16e99e7ea3683f91e2
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-11-03 14:05:30 +0000

    add sobre page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
-import Cadastro from "./pages/cadastro";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SobrePage from "./pages/Sobre";
+import Layout from "./components/Layout"; // 1. Importe o Layout
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/index" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* 2. Crie uma rota "pai" que usa o Layout */}
+      <Route element={<Layout />}>
+        {/* 3. Coloque suas páginas como "filhas" do Layout */}
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<SobrePage />} />
 
+        {/* Outras páginas futuras também irão aqui dentro */}
+      </Route>
+    </Routes>
   );
 }
 
-
-
-export default App;// src/App.tsx
-
+export default App;
